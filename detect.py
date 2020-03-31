203159cc5b7dccf48f2da27fc3780cd9fe8951ef
Daniel Pareja <ParejaDan@gmail.com>
2020-03-30 22:05:26 -0400

    convert ' to " in detect.py

--- detect.py
+++ detect.py
@@ -9,20 +9,20 @@ from bopflow.transform import transform_images
 from bopflow.const import DEFAULT_IMAGE_SIZE, COCO_DEFAULT_CLASSES
 from bopflow.iomanage import draw_outputs, load_random_tfrecord_dataset, load_image_file
 
-flags.DEFINE_string('classes', './data/coco.names', 'path to classes file')
-flags.DEFINE_string('weights', './checkpoints/yolov3.tf',
-                    'path to weights file')
-flags.DEFINE_boolean('tiny', False, 'yolov3 or yolov3-tiny')
-flags.DEFINE_integer('size', 416, 'resize images to')
-flags.DEFINE_string('image', './data/girl.png', 'path to input image')
-flags.DEFINE_string('tfrecord', None, 'tfrecord instead of image')
-flags.DEFINE_string('output', './output.jpg', 'path to output image')
-flags.DEFINE_integer('num_classes', 80, 'number of classes in the model')
+flags.DEFINE_string("classes", "./data/coco.names", "path to classes file")
+flags.DEFINE_string("weights", "./checkpoints/yolov3.tf",
+                    "path to weights file")
+flags.DEFINE_boolean("tiny", False, "yolov3 or yolov3-tiny")
+flags.DEFINE_integer("size", 416, "resize images to")
+flags.DEFINE_string("image", "./data/girl.png", "path to input image")
+flags.DEFINE_string("tfrecord", None, "tfrecord instead of image")
+flags.DEFINE_string("output", "./output.jpg", "path to output image")
+flags.DEFINE_integer("num_classes", 80, "number of classes in the model")
 
 
 def main(_argv):
     yolo = coco_yolo_detector(use_tiny=FLAGS.tiny)
-    logging.info('weights loaded')
+    logging.info("weights loaded")
 
     if FLAGS.tfrecord:
         img_raw, _ = load_random_tfrecord_dataset(FLAGS.tfrecord, FLAGS.classes)
@@ -35,19 +35,19 @@ def main(_argv):
     t1 = time.time()
     detections = yolo.evaluate(img)
     t2 = time.time()
-    logging.info('time: {}'.format(t2 - t1))
+    logging.info("time: {}".format(t2 - t1))
 
-    logging.info('detections:')
+    logging.info("detections:")
     for detected_class in detections:
         logging.info(detected_class)
 
     img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
     img = draw_outputs(img, detections)
     cv2.imwrite(FLAGS.output, img)
-    logging.info('output saved to: {}'.format(FLAGS.output))
+    logging.info("output saved to: {}".format(FLAGS.output))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         app.run(main)
     except SystemExit:
